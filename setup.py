f5321b3b60bf1e4e775a0006e2bb90a77e3a550c
George <noemail@noemail.noemail>
2020-08-06 17:03:01 +0300

    Making it run with Python3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         "gevent",
         "netifaces",
         "scapy",
-        "py2-ipaddress"
+        "ipaddress"
     ],
     dependency_links=[
         "http://www.secdev.org/projects/scapy/files/",
